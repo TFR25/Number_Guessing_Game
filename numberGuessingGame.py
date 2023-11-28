6749a3a3fda6937ffd90bdd5dde12518630397a8
Tasha Fernandez-Ross <tfernandez25@gmail.com>
2023-11-27 20:15:10 -0800

    add hints

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,27 +1,32 @@
 import random
 from art import logo
+
 print(logo)
 
-EASY_LEVEL = 5
-HARD_LEVEL = 10
+EASY_LEVEL = 10
+HARD_LEVEL = 5
+
 print("Welcome to the number guessing game! \nI'm thinking of a number between 1 and 100.")
 
 def guess_a_num():
     num = random.randint(0, 100)
-    print(num)
     lives = tries
-    print(lives)
-    guess = input("Choose a number between 0 and 100: ")
+    guess = int(input("Choose a number between 0 and 100: "))
     while guess != num:
         lives -= 1
+        if guess > num:
+            print("Too high.")
+        elif guess < num:
+            print("Too low.")
+        print(f"You have {lives} lives left.")
         if lives > 0:
-            guess = int(input((f"Your guess is {guess} and is incorrect. Guess another number: ")))
+            guess = int(input((f"Guess another number: ")))
         elif guess == num:
             print("Well done. You guessed correctly.")
         else:
-            print("Sorry, you lose.")
+            print(f"Sorry, you lose.\nThe answer was {num}")
             return
-    
+   
 def start():
     level = input("Choose a level. Type 'easy' or 'hard': ")
     if level == "easy":
